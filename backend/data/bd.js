5c2fb9fafe8937999c0fbcbc3297d9fdca4c57a3
Carolina <mendezc@ambiente.gba.gob.ar>
2024-06-25 10:45:43 -0300

    se hizo la conexion de la base de datos subida a Alwaysdata

--- backend/data/bd.js
+++ backend/data/bd.js
@@ -1,10 +1,20 @@
 const {Sequelize} = require ("sequelize")
+require('dotenv').config();
 
 
 /* nombre de la base datos - user - contraseña - {donde esta alojada la bd?, lenguaje,puerto} */
-const db = new Sequelize ("viajar2024","root","1234",{
-    host: "localhost",
+// const db = new Sequelize ("viajar2024","root","1234",{
+//     host: "localhost",
+//     dialect: "mysql",
+//     port:3307
+// })
+
+const db = new Sequelize ("viaja2024_viajar2024",process.env.DB_USER,process.env.DB_PASSWORD,{
+    host: process.env.DB_HOST,
     dialect: "mysql",
-    port:3307
+    port:3306
 })
+
 module.exports = db
+
+// INSERT INTO `usuarios` (`idusuarios`, `nombre`, `apellido`, `mail`, `password`, `createdAt`, `updatedAt`) VALUES (NULL, 'carolina', 'mendez', 'c@o.com', '111', NULL, NULL);
